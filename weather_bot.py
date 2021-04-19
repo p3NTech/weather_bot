ae1838f19d60359fb8ac8c89b6db696071db7e98
p3ntech <44126273+p3NTech@users.noreply.github.com>
2021-04-19 21:18:24 +0500

    Update weather_bot.py

--- weather_bot.py
+++ weather_bot.py
@@ -15,6 +15,7 @@ message2 = ['Сообщение1', 'Сообщение2', 'Сообщение3',
 message3 = ['Сообщение1', 'Сообщение2', 'Сообщение3', 'Сообщение4','Сообщение5']
 message4 = ['Сообщение1', 'Сообщение2', 'Сообщение3', 'Сообщение4','Сообщение5']
 message5 = ['Сообщение1', 'Сообщение2', 'Сообщение3', 'Сообщение4','Сообщение5']
+
 random_message1 = lambda: random.choice(message1)
 random_message2 = lambda: random.choice(message2)
 random_message3 = lambda: random.choice(message3)
