0b5ab2c399f4d67b1f2214a46336ccd6ffe3e88e
p3ntech <44126273+p3NTech@users.noreply.github.com>
2021-04-19 21:24:52 +0500

    Update weather_bot.py

--- weather_bot.py
+++ weather_bot.py
@@ -3,8 +3,8 @@ import telebot
 import random
 
 url = 'http://api.openweathermap.org/data/2.5/weather' #open weather url
-api_open_weather = '966cc6ce89188b2bc797546a3487bf55'#ключ open weather api
-api_telegram_token = '1687680321:AAHm7NH2ZsagoBAlcfNSV8U5Wwj0U9jhHK0' #токен telegram api
+api_open_weather = ''#ключ open weather api
+api_telegram_token = '' #токен telegram api
 bot = telebot.TeleBot(api_telegram_token)
 print("")
 print("Weather bot [Telegram: @p3ntech]") #сообщение в консоль
