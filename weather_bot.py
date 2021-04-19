7493792922c9b08c5b99ec8bebd21e8936df2cad
p3ntech <44126273+p3NTech@users.noreply.github.com>
2021-04-19 21:18:03 +0500

    Update weather_bot.py

--- weather_bot.py
+++ weather_bot.py
@@ -10,8 +10,16 @@ print("")
 print("Weather bot [Telegram: @p3ntech]") #сообщение в консоль
 print("initialize") #сообщение в консоль
 
-message15 = ['Сообщение1', 'Сообщение2', 'Сообщение3', 'Сообщение4','Сообщение5']
-random_message = lambda: random.choice(message15)
+message1 = ['Сообщение1', 'Сообщение2', 'Сообщение3', 'Сообщение4','Сообщение5']
+message2 = ['Сообщение1', 'Сообщение2', 'Сообщение3', 'Сообщение4','Сообщение5']
+message3 = ['Сообщение1', 'Сообщение2', 'Сообщение3', 'Сообщение4','Сообщение5']
+message4 = ['Сообщение1', 'Сообщение2', 'Сообщение3', 'Сообщение4','Сообщение5']
+message5 = ['Сообщение1', 'Сообщение2', 'Сообщение3', 'Сообщение4','Сообщение5']
+random_message1 = lambda: random.choice(message1)
+random_message2 = lambda: random.choice(message2)
+random_message3 = lambda: random.choice(message3)
+random_message4 = lambda: random.choice(message4)
+random_message5 = lambda: random.choice(message5)
 
 @bot.message_handler(commands=['start']) #старт
 def welcome(message):
@@ -37,20 +45,20 @@ def test(message):
         weather = result.json() #экспорт параметров
 
         if weather["main"]['temp'] < -10:   #при -10
-            status = "На улице холодно, одевайтесь теплее! Чтобы согреться, могу предложить Вам сходить в ресторан и выпить чашечку кофе!" 
-            #bot.send_photo(message.chat.id, 'ссылка на изображение', "")
+            status = random_message1 # 
+            #bot.send_photo(message.chat.id, 'ссылка на изображение', "") # изображение1
         elif weather["main"]['temp'] < 0:   #при 0
-            status = "Мороз и солнце, день чудесный! Самое время выпить глинтвейн!"
-            #bot.send_photo(message.chat.id, 'ссылка на изображение', "")
+            status = random_message2 # 
+            #bot.send_photo(message.chat.id, 'ссылка на изображение', "") # изображение2
         elif weather["main"]['temp'] < 10:  #при +10
-            status = "Погода шепчет, что сегодня вам нужно сходить на каток! Но стоит одеть шапку и шарф!"
-            #bot.send_photo(message.chat.id, 'ссылка на изображение', "")
+            status = random_message3 # 
+            #bot.send_photo(message.chat.id, 'ссылка на изображение', "") # изображение3
         elif weather["main"]['temp'] < 30:  #при +30
-            status = "Пейте много воды, на улице жарко!"
-            #bot.send_photo(message.chat.id, 'ссылка на изображение', "")
+            status = random_message4 # 
+            #bot.send_photo(message.chat.id, 'ссылка на изображение', "") # изображение4
         else:   #при +30+
-            status = "Не выходите на улицу без надобности и пейте много воды, на улице жарко!"
-            #bot.send_photo(message.chat.id, 'ссылка на изображение', "")
+            status = random_message5 # 
+            #bot.send_photo(message.chat.id, 'ссылка на изображение', "") # изображение5
 
         bot.send_message(message.chat.id, "Сейчас в городе " + str(weather["name"]) + " температура " +
                          str(weather["main"]['temp']) + "°C" + "\n" +
