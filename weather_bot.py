d7c8c07a26aa2f9564007b110aa016209806d91b
p3ntech <44126273+p3NTech@users.noreply.github.com>
2021-04-23 19:12:43 +0500

    Update weather_bot.py

--- weather_bot.py
+++ weather_bot.py
@@ -6,7 +6,19 @@ import random
 print("")
 print("initialize") #сообщение в консоль
 
-print("constants.random_message4")
+message1 = ['Сообщение1', 'Сообщение2', 'Сообщение3', 'Сообщение4','Сообщение5']
+message2 = ['Сообщение1', 'Сообщение2', 'Сообщение3', 'Сообщение4','Сообщение5']
+message3 = ['Сообщение1', 'Сообщение2', 'Сообщение3', 'Сообщение4','Сообщение5']
+message4 = ['1', '2', '3', '4','5']
+message5 = ['Сообщение1', 'Сообщение2', 'Сообщение3', 'Сообщение4','Сообщение5']
+
+random_message1 = lambda: random.choice(message1)
+random_message2 = lambda: random.choice(message2)
+random_message3 = lambda: random.choice(message3)
+random_message4 = random.choice(message4)
+random_message5 = lambda: random.choice(message5)
+
+print("random_message4")
 
 bot = telebot.TeleBot(constants.api_telegram_token)
 
