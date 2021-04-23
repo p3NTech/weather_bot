9a7988dc5bf1b817acc2f31ab5c4179dec2de4fc
p3ntech <44126273+p3NTech@users.noreply.github.com>
2021-04-23 19:23:50 +0500

    Update weather_bot.py

--- weather_bot.py
+++ weather_bot.py
@@ -36,19 +36,19 @@ def test(message):
 
         if weather["main"]['temp'] < -10:   #при -10
             status = "На улице холодно, одевайтесь теплее! Чтобы согреться, могу предложить Вам сходить в ресторан и выпить чашечку кофе!" 
-	bot.send_photo(message.chat.id, 'http://f0535055.xsph.ru/1/pog2.jpg', random_message1 ) #
+	bot.send_photo(message.chat.id, 'http://f0535055.xsph.ru/1/pog2.jpg', ""+random_message1 ) #
         elif weather["main"]['temp'] < 0:   #при 0
             status = "Мороз и солнце, день чудесный! Самое время выпить глинтвейн!"
-	bot.send_photo(message.chat.id, 'http://f0535055.xsph.ru/1/pog2.jpg', random_message2 ) #
+	bot.send_photo(message.chat.id, 'http://f0535055.xsph.ru/1/pog2.jpg', ""+random_message2 ) #
         elif weather["main"]['temp'] < 10:  #при +10
             status = "Погода шепчет, что сегодня вам нужно сходить на каток! Но стоит одеть шапку и шарф!"
-	bot.send_photo(message.chat.id, 'http://f0535055.xsph.ru/1/pog2.jpg', random_message3 ) #
+	bot.send_photo(message.chat.id, 'http://f0535055.xsph.ru/1/pog2.jpg', ""+random_message3 ) #
         elif weather["main"]['temp'] < 30:  #при +30
             status = "Пейте много воды, на улице жарко!"
-		bot.send_photo(message.chat.id, 'http://f0535055.xsph.ru/1/pog2.jpg', random_message4 ) #
+		bot.send_photo(message.chat.id, 'http://f0535055.xsph.ru/1/pog2.jpg', ""+random_message4 ) #
         else:   #при +30+
             status = "Не выходите на улицу без надобности и пейте много воды, на улице жарко!"
-	bot.send_photo(message.chat.id, 'http://f0535055.xsph.ru/1/pog2.jpg', random_message5 ) #
+	bot.send_photo(message.chat.id, 'http://f0535055.xsph.ru/1/pog2.jpg', ""+random_message5 ) #
 
         bot.send_message(message.chat.id, "Сейчас в городе " + str(weather["name"]) + " температура " +
                          str(weather["main"]['temp']) + "°C" + "\n" +
