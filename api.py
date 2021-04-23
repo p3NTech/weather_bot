301fc250e12d6cacb92141ffda5a5c798d09f568
p3ntech <44126273+p3NTech@users.noreply.github.com>
2021-04-23 19:36:03 +0500

    Update api.py

--- api.py
+++ api.py
@@ -1,5 +1,3 @@
-import random
-
 url = 'http://api.openweathermap.org/data/2.5/weather' #open weather url
 api_open_weather = '966cc6ce89188b2bc797546a3487bf55'#ключ open weather api
 api_telegram_token = '1687680321:AAHm7NH2ZsagoBAlcfNSV8U5Wwj0U9jhHK0' #токен telegram api
