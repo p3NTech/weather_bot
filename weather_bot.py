3152d452dcfca574a7c83722525fedb6fbddc588
p3ntech <44126273+p3NTech@users.noreply.github.com>
2021-04-23 19:49:04 +0500

    Update weather_bot.py

--- weather_bot.py
+++ weather_bot.py
@@ -48,9 +48,7 @@ def test(message):
 	bot.send_photo(message.chat.id, 'https://darkside.guru/files/404city.png', "Сейчас в городе " + str(weather["name"]) + " температура " +
                          str(weather["main"]['temp']) + "°C" + "\n" +
                          "Влажность: " + str(int(weather['main']['humidity'])) + "%" + "\n" +
-                         "На улице сейчас " + str(weather['weather'][0]["description"]) + "\n"+
-                         "-------------------------------------------------------------------"
-                         "\n")
+                         "На улице сейчас " + str(weather['weather'][0]["description"])
         else:   #при +30+
             status = random_message5 #"Не выходите на улицу без надобности и пейте много воды, на улице жарко!"
 
