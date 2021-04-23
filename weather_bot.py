c3e32955751e86f88e8368b081ab59d9ea944910
p3ntech <44126273+p3NTech@users.noreply.github.com>
2021-04-23 19:16:30 +0500

    Update weather_bot.py

--- weather_bot.py
+++ weather_bot.py
@@ -1,6 +1,6 @@
 import requests
 import telebot
-import constants
+import api
 import random
 
 print("")
@@ -20,7 +20,7 @@ random_message5 = lambda: random.choice(message5)
 
 print(random_message4)
 
-bot = telebot.TeleBot(constants.api_telegram_token)
+bot = telebot.TeleBot(api.api_telegram_token)
 
 @bot.message_handler(commands=['start']) #старт
 def welcome(message):
@@ -38,19 +38,24 @@ def test(message):
 
         if weather["main"]['temp'] < -10:   #при -10
             status = "На улице холодно, одевайтесь теплее! Чтобы согреться, могу предложить Вам сходить в ресторан и выпить чашечку кофе!" 
-            bot.send_photo(message.chat.id, 'ссылка на изображение', "")
+	bot.send_photo(message.chat.id, 'http://f0535055.xsph.ru/1/pog2.jpg', "🌡Сейчас в городе " + str(weather["name"]) + " температура " +
+str(weather["main"]['temp']) + "°C" + "\n" + "💦Влажность: " + str(int(weather['main']['humidity'])) + "%" + "\n" + "🏘На улице сейчас " + str(weather['weather'][0]["description"]) + "\n"+"Очень холодно, сидим дома!" ) #
         elif weather["main"]['temp'] < 0:   #при 0
             status = "Мороз и солнце, день чудесный! Самое время выпить глинтвейн!"
-            bot.send_photo(message.chat.id, 'ссылка на изображение', "")
+	bot.send_photo(message.chat.id, 'http://f0535055.xsph.ru/1/pog2.jpg', "🌡Сейчас в городе " + str(weather["name"]) + " температура " +
+str(weather["main"]['temp']) + "°C" + "\n" + "💦Влажность: " + str(int(weather['main']['humidity'])) + "%" + "\n" + "🏘На улице сейчас " + str(weather['weather'][0]["description"]) + "\n"+"Очень холодно, сидим дома!" ) #
         elif weather["main"]['temp'] < 10:  #при +10
             status = "Погода шепчет, что сегодня вам нужно сходить на каток! Но стоит одеть шапку и шарф!"
-            bot.send_photo(message.chat.id, 'ссылка на изображение', "")
+	bot.send_photo(message.chat.id, 'http://f0535055.xsph.ru/1/pog2.jpg', "🌡Сейчас в городе " + str(weather["name"]) + " температура " +
+str(weather["main"]['temp']) + "°C" + "\n" + "💦Влажность: " + str(int(weather['main']['humidity'])) + "%" + "\n" + "🏘На улице сейчас " + str(weather['weather'][0]["description"]) + "\n"+"Очень холодно, сидим дома!" ) #
         elif weather["main"]['temp'] < 30:  #при +30
             status = "Пейте много воды, на улице жарко!"
-            bot.send_photo(message.chat.id, 'ссылка на изображение', "")
+		bot.send_photo(message.chat.id, 'http://f0535055.xsph.ru/1/pog2.jpg', "🌡Сейчас в городе " + str(weather["name"]) + " температура " +
+str(weather["main"]['temp']) + "°C" + "\n" + "💦Влажность: " + str(int(weather['main']['humidity'])) + "%" + "\n" + "🏘На улице сейчас " + str(weather['weather'][0]["description"]) + "\n"+"Очень холодно, сидим дома!" ) #
         else:   #при +30+
             status = "Не выходите на улицу без надобности и пейте много воды, на улице жарко!"
-            bot.send_photo(message.chat.id, 'ссылка на изображение', "")
+bot.send_photo(message.chat.id, 'http://f0535055.xsph.ru/1/pog2.jpg', "🌡Сейчас в городе " + str(weather["name"]) + " температура " +
+str(weather["main"]['temp']) + "°C" + "\n" + "💦Влажность: " + str(int(weather['main']['humidity'])) + "%" + "\n" + "🏘На улице сейчас " + str(weather['weather'][0]["description"]) + "\n"+"Очень холодно, сидим дома!" ) #
 
         bot.send_message(message.chat.id, "Сейчас в городе " + str(weather["name"]) + " температура " +
                          str(weather["main"]['temp']) + "°C" + "\n" +
