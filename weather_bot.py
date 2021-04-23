24d3ac22a240e43e82532a45411cccabd10d75cd
p3ntech <44126273+p3NTech@users.noreply.github.com>
2021-04-23 19:02:32 +0500

    Update weather_bot.py

--- weather_bot.py
+++ weather_bot.py
@@ -5,45 +5,43 @@ import constants
 print("")
 print("initialize") #сообщение в консоль
 
-bot = telebot.TeleBot(constants.api_telegram_token)
-
-@bot.message_handler(commands=['start']) #старт
+@bot.message_handler(commands=['start'])#старт
 def welcome(message):
-    bot.send_message(message.chat.id, f'Привет!  {message.from_user.first_name}'
-                                      f' напиши название города и узнай погоду в нём. \n' 
-                                      f'Для помощи напиши "/help"') #Сообщение при запуске
+    bot.send_message(message.chat.id, f'Привет!  {message.from_user.first_name} \n'
+                                      f' напиши название города и узнай погоду в нём. "/help"')#Сообщение при запуске
 
 
 @bot.message_handler(commands=['help'])
 def welcome(message):
     bot.send_message(message.chat.id,
                      f'/start запуск бота \n'
-                     ) #сообщение(ответ) на команду /help
+                     )#сообщение(ответ) на команду /help
 
 
-@bot.message_handler(content_types=['text']) #обработчик
+@bot.message_handler(content_types=['text'])#обработчик
 def test(message):
     city_name = message.text
 
     try:
-        params = {'APPID': constants.api_open_weather, 'q': city_name, 'units': 'metric', 'lang': 'ru'}
-        result = requests.get(constants.url, params=params) #параметры api open weather
-        weather = result.json() #экспорт параметров
+        params = {'APPID': api_open_weather, 'q': city_name, 'units': 'metric', 'lang': 'ru'}
+        result = requests.get(url, params=params)#параметры api open weather
+        weather = result.json()#экспорт параметров
 
-        if weather["main"]['temp'] < -10:   #при -15
-            status = "На улице холодно, одевайтесь теплее! Чтобы согреться, могу предложить Вам сходить в ресторан и выпить чашечку кофе! (здесь может быть ваша реклама ресторана)" # random_message1 # 
-            #bot.send_photo(message.chat.id, 'ссылка на изображение', "") # изображение1
-	elif weather["main"]['temp'] < 0:   #при 0
-            status = "Погода радует!... Можно уже менять зимнюю резину на летнюю)) (реклама сто, шиномонтаж)"  # random_message2 # 
-            #bot.send_photo(message.chat.id, 'ссылка на изображение', "") # изображение2
-	elif weather["main"]['temp'] < 10:   #при 0, при -5 не дает запустить бот
-            status = "На улице становится теплее. Советую Вам сегодня выйти на пробежку. А чтобы было комфортно бегать, можно приобрести зимние кроссовки в интернет-магазине. (реклама интернет магазина и фото кроссовок, которые у них продаются)"
-        elif weather["main"]['temp'] < 30:  #при +10
-            status = "На улице заметно теплее! Уличные тренировки в такую погоду самое то! Спортивный инвентарь и подходящую одежду можно посмотреть по ссылке ниже! (реклама шоу-рума или магазина спорттоваров)" # random_message3 # 
-            #bot.send_photo(message.chat.id, 'ссылка на изображение', "") # изображение3
+        if weather["main"]['temp'] < -10:   #при -10
+            status = "На улице холодно, одевайтесь теплее! Чтобы согреться, могу предложить Вам сходить в ресторан и выпить чашечку кофе!" 
+            bot.send_photo(message.chat.id, 'ссылка на изображение', "")
+        elif weather["main"]['temp'] < 0:   #при 0
+            status = "Мороз и солнце, день чудесный! Самое время выпить глинтвейн!"
+            bot.send_photo(message.chat.id, 'ссылка на изображение', "")
+        elif weather["main"]['temp'] < 10:  #при +10
+            status = "Погода шепчет, что сегодня вам нужно сходить на каток! Но стоит одеть шапку и шарф!"
+            bot.send_photo(message.chat.id, 'ссылка на изображение', "")
+        elif weather["main"]['temp'] < 30:  #при +30
+            status = "Пейте много воды, на улице жарко!"
+            bot.send_photo(message.chat.id, 'ссылка на изображение', "")
         else:   #при +30+
-            status = "Предлагаю ознакомиться с топовыми моделями купальников в этом сезоне! Только сегодня вы можете стать обладательницей шикарного бикини!(реклама магазина купальников)" # random_message5 #
-            #bot.send_photo(message.chat.id, 'ссылка на изображение', "") # изображение5
+            status = "Не выходите на улицу без надобности и пейте много воды, на улице жарко!"
+            bot.send_photo(message.chat.id, 'ссылка на изображение', "")
 
         bot.send_message(message.chat.id, "Сейчас в городе " + str(weather["name"]) + " температура " +
                          str(weather["main"]['temp']) + "°C" + "\n" +
@@ -51,11 +49,11 @@ def test(message):
                          "На улице сейчас " + str(weather['weather'][0]["description"]) + "\n"+
                          "-------------------------------------------------------------------"
                          "\n" + status)
+
     except:
-	
-        bot.send_photo(message.chat.id, 'https://darkside.guru/files/404city.png', "Город " + city_name + " не найден ") # сообщение в случае если город не найден
+        bot.send_photo(message.chat.id, 'ссылка на изображение', "Город " + city_name + " не найден") # сообщение в случае если город не найден
 		
-print("Started!")
+print("Started!")#сообщение в консоль
 bot.polling(none_stop=True)
 print("")
-print("Stoped!")
+print("Stoped!")#сообщение в консоль
